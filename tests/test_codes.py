ac7c314074898e72ddc06cb7bf857eec2e9a7ecf
apoorvapatil29 <41383504+apoorvapatil29@users.noreply.github.com>
2024-08-22 23:12:53 +0530

    Update test_codes.py

--- tests/test_codes.py
+++ tests/test_codes.py
@@ -4,13 +4,13 @@ from src.codes import add_numbers, multiply_numbers
 import pytest
 from src.codes import add_numbers, multiply_numbers
 
-def test_add_numbers():
-    assert add_numbers(2, 3) == 5
-    assert add_numbers(-1, 1) == 0
-    assert add_numbers(0, 0) == 0
-    assert add_numbers(2, 2) == 4
-def test_multiply_numbers():
-    assert multiply_numbers(2, 3) == 6
-    assert multiply_numbers(-1, 1) == -1
-    assert multiply_numbers(0, 5) == 0
-    #assert multiply_numbers(0, 0) == 5
+# def test_add_numbers():
+#     assert add_numbers(2, 3) == 5
+#     assert add_numbers(-1, 1) == 0
+#     assert add_numbers(0, 0) == 0
+#     assert add_numbers(2, 2) == 4
+# def test_multiply_numbers():
+#     assert multiply_numbers(2, 3) == 6
+#     assert multiply_numbers(-1, 1) == -1
+#     assert multiply_numbers(0, 5) == 0
+#     #assert multiply_numbers(0, 0) == 5
