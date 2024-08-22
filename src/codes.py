c6432379a97b6b875969136b6b2c2818ebd0c31f
apoorvapatil29 <41383504+apoorvapatil29@users.noreply.github.com>
2024-08-22 19:46:01 +0530

    Update codes.py

--- src/codes.py
+++ src/codes.py
@@ -5,7 +5,7 @@ def add_numbers(a, b):
 def multiply_numbers(a, b):
     """Returns the product of two numbers."""
     return a * b
-
-def multiply_numbers(a, b):
-    """Returns the product of two numbers."""
-    return a * b
+    
+def subtract_numbers(a, b):
+    """Returns the subtraction of two numbers."""
+    return a - b
